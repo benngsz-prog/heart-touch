49d22d57fa42466ecc5de0461d1fa7bc458081fa
benngsz-prog <benngsz@foxmail.com>
2026-07-10 22:01:20 +0800

    创建 sw.js

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = "heart-touch-v1";
+
+
+const FILES = [
+    "/",
+    "/index.html",
+    "/style.css",
+    "/app.js"
+];
+
+
+self.addEventListener(
+"install",
+event=>{
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+        .then(cache=>{
+
+            return cache.addAll(FILES);
+
+        })
+
+    );
+
+});
+
+
+
+self.addEventListener(
+"fetch",
+event=>{
+
+    event.respondWith(
+
+        caches.match(event.request)
+        .then(response=>{
+
+            return response ||
+            fetch(event.request);
+
+        })
+
+    );
+
+});
